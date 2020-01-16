f12716c1d49a978844dae16ded09d9fd782aef21
Saurabh Jain <saurabhjain291197@gmail.com>
2020-01-16 13:30:43 +0530

    changes

--- bot.js
+++ bot.js
@@ -35,8 +35,8 @@ var JumperBot = function () {
         swingBot = false,     
         relAngleX, relAngleY, relFiredPointX, relFiredPointY; 
 
-    canvas.width = 400;
-    canvas.height = 500;
+    canvas.width = 1400;
+    canvas.height = 900;
 
     context = canvas.getContext('2d');
     context.lineWidth = ropeThickness; 
